283b187c32ca1c34f85ab02f324581bf6a66e733
dingpeng <dingpeng@ebupt.com>
2018-06-28 18:36:16 +0800

    [提交]dev分支

--- src/test/java/example/OneTest.java
+++ src/test/java/example/OneTest.java
@@ -8,7 +8,6 @@ public class OneTest {
   public void testFoo() throws Exception {
     One one = new One();
     //Test foo
-    System.out.println("master分支");
     System.out.println("dev分支");
     assertEquals("foo", one.foo());
   }
